d242a13925dc018a265fa714e3fc0eae72cbcb18
chuckgstanley <cgs2138@columbia.edu>
2014-02-21 22:42:44 -0500

    linked jquery and html divs

--- week4assignment.js
+++ week4assignment.js
@@ -1,3 +1,25 @@
 /**
  * @author Peterphobia
  */
+//This will tell me when my page is loaded
+function dataLoaded(){
+console.log ("Document is Loaded");	
+	
+}
+
+//Set up a jquery div
+var jqdiv = $("<div>");
+
+//This lets me know that I can see the text in my jquery div
+$(jqdiv).html("Jquery Displayed.");
+
+
+//Put jqdiv into html div
+//I should see "jquery displayed"
+$("#javaEndsUpHere").append(jqdiv);
+
+
+$( document ).ready(dataLoaded);
+
+//This will tell me that my java is being displayed
+console.log("Javascript working");
