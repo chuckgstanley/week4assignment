dbb871692568010e119a4194c134605e023b7667
chuckgstanley <cgs2138@columbia.edu>
2014-02-21 23:48:38 -0500

    broken program trying to fix

--- week4assignment.js
+++ week4assignment.js
@@ -2,7 +2,15 @@
  * @author Peterphobia
  */
 //This will tell me when my page is loaded
-function dataLoaded(){
+
+function loadedJSON(myRecords){
+console.log the "name" property of myRecords
+
+console log statement should output Sgt. Pepper
+
+console.log(myRecords.myRecords.name[0]);	
+
+
 console.log ("Document is Loaded");	
 	
 }
@@ -10,14 +18,17 @@ console.log ("Document is Loaded");
 //Set up a jquery div
 var jqdiv = $("<div>");
 
-//This lets me know that I can see the text in my jquery div
-$(jqdiv).html("Jquery Displayed.");
+//add record to div
+$(jqdiv).html(myRecords.myRecords[0].name);
 
 
 //Put jqdiv into html div
 //I should see "jquery displayed"
 $("#javaEndsUpHere").append(jqdiv);
+//use the jquery function to get myRecords file
+//get the name, function, and type of file
 
+$.get("myRecords.json"), loadedJSON, "json"
 
 $( document ).ready(dataLoaded);
 
